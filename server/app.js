a4ead9c9236df89daa306caefc9790c4a0b751f6
Burak Bayraktar <byrktrhmx@gmail.com>
2022-04-14 03:17:03 +0300

    SERVER: HTTP codes added

--- server/app.js
+++ server/app.js
@@ -7,39 +7,50 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors())
+app.use(cors());
 app.use(async (req) => {
   try {
     const result = await LoginToSystem();
     req.headers.authorization = result.headers["set-cookie"];
     req.next();
   } catch (error) {
-      console.log(error)
+    res.json({ authError: "You're not authorized" });
+    res.status(401);
   }
 });
 
 app.get("/machines", async (req, res) => {
-    try {
-        if (!req.headers.authorization) 
-          res.json({authError: "You're not authorized"})  
-            
-        const { data } = await client(req).query({query: FETCH_MACHINES})
-        res.json(data.entities)
-    } catch (err) {
-      console.log(err);
+  try {
+    if (!req.headers.authorization) {
+      res.json({ authError: "You're not authorized" });
+      res.status(401);
     }
-  });
 
+    const { data } = await client(req).query({ query: FETCH_MACHINES });
+    res.json(data.entities);
+    res.status(200);
+  } catch (err) {
+    res.status(404).send("Something went wrong...");
+  }
+});
 
-
-app.get("/machine/:id", async (req,res) => {
-    try {
-        const { data } = await client(req).query({query: FETCH_MACHINE, variables: {id: req.params.id}})
-        res.json(data)
-    } catch (error) {
-        console.log(error)
+app.get("/machine/:id", async (req, res) => {
+  try {
+    if (!req.headers.authorization) {
+      res.json({ authError: "You're not authorized" });
+      res.status(401);
     }
-})
+
+    const { data } = await client(req).query({
+      query: FETCH_MACHINE,
+      variables: { id: req.params.id },
+    });
+
+    res.json(data);
+  } catch (error) {
+    res.status(404).send("Something went wrong...");
+  }
+});
 
 server.listen(process.env.PORT || 5000, () => {
   console.log("*5000, listening...");
